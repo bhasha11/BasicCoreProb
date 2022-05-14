74e1cb6e3d98b1d78ded5a2bb7f7d2787fc97846
anjali <anjalimalviya2532@gmail.com>
2022-05-14 15:00:46 +0530

    [Anjali]Add: To find prime factors of a number

---
+++ PrimeFactorization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblem1
+{
+    internal class PrimeFactorization
+    {
+        //define variable
+        public int j = 0;
+        public int num = 0;
+        public int factorial = 1;
+        public void CalcSFactorialSeries()
+        {
+            Console.Write("Calculate the factorial of a given number:"); //Input From User
+            num = Convert.ToInt32(Console.ReadLine());//reading input form user
+            for (j = 1; j <= num; j++) //Checking j is <= num example(1<5) and den incrementing it by 1 
+            factorial *= j; //ca;lculate  factorial = factorial * j example (factroial = 1*1 or 2*2 or 5*5)
+            Console.Write("The Factorial of {0} is: {1}\n", num, factorial); //op
+        }
+    }
+}
+
+ 
